8a78aa590a365a3b34be411ede23d33ff6682a58
Moomimia <thu.le@student.samk.fi>
2023-02-26 19:46:39 +0200

    ReplaceVeryKindofworking

--- # Very-replacer-program:.py
+++ # Very-replacer-program:.py
@@ -1,25 +1,43 @@
 # Very-replacer-program: 
 # 1. Reading the English essayÖ To read the English essay from a .txt file, you can use the open() function to open the file and the read() method to read its contents.
-# Keywords: Python open file, Python read file.
+# Keysplit_text: Python open file, Python read file.
 
 file = open('words.txt', 'r')
 text = file.read()
-print(text)
+#print(text)
 
 # 2. Defining the replacements
 # To define the replacements for the word "very", you can use a dictionary that maps the word to its replacement. 
-# Keywords: Python dictionary, Python mapping
+# Keysplit_text: Python dictionary, Python mapping
 
 # Using curly braces
 my_dict = {'very calm': 'patient', 'very big': 'huge', 'very little': 'tiny'}
-print(my_dict)
+#print(my_dict)
 
 #3. Replacing the word "very" with its corresponding replacement
-# To replace the word "very" with its corresponding replacement, you can split the text into words, iterate over each word, and replace it with its replacement if it is in the dictionary.
-# Keywords: Python string split, Python for loop, Python if-else statement, .split(),  .lower(), .rstrip()
+# To replace the word "very" with its corresponding replacement, you can split the text into split_text, iterate over each word, and replace it with its replacement if it is in the dictionary.
+# Keysplit_text: Python string split, Python for loop, Python if-else statement, .split(),  .lower(), .rstrip()
 # NOTE: there is many many ways to do this. No one correct answer.
 
 split_text = text.split(" ")
-print(split_text)
+# print(split_text)
+
+# Replace the word "very ..." with "..."
+for i in range(len(split_text)):
+    if split_text[i] == "calm":
+        split_text[i] = "patient"
+
+    if split_text[i] == "big":
+        split_text[i] = "huge"
+
+    if split_text[i] == "little":
+        split_text[i] = "tiny"
+    
+    if split_text[i] == "very":
+        split_text[i] = ""
+
+new_text = " ".join(split_text)
+print(new_text)
+
 
 
