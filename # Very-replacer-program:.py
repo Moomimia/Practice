29796d5dfc5f2e7fbdb81c262824b50610c1ee54
Moomimia <thu.le@student.samk.fi>
2023-02-26 18:51:55 +0200

    FirstStep

---
+++ # Very-replacer-program:.py
@@ -0,0 +1,3 @@
+# Very-replacer-program: 
+# 1. Reading the English essayÖ To read the English essay from a .txt file, you can use the open() function to open the file and the read() method to read its contents.
+# Keywords: Python open file, Python read file.
